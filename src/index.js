a70686316404b537d3698e1ba189b387dbf31335
Victor Shahbazian <victor.sh.91@gmail.com>
2019-07-29 06:44:56 -0400

    Boilerplate working with basic functionality

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import _ from 'lodash'
+import * as PIXI from 'pixi.js'
+import Trash from './img/trash.png'
+import './css/index.scss'
+
+function component() {
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
+
+const trashImg = document.createElement('img');
+trashImg.src = Trash;
+
+// document.body.appendChild(trashImg);
+
+const app = new PIXI.Application();
+document.body.appendChild(app.view);
+app.loader.add('trash', Trash).load((loader, resources) => {
+  const trash = new PIXI.Sprite(resources.trash.texture);
+
+  trash.x = app.renderer.width / 2;
+  trash.y = app.renderer.height / 2;
+
+  trash.anchor.x = 0.5;
+  trash.anchor.y = 0.5;
+
+  app.stage.addChild(trash);
+
+  app.ticker.add(() => {
+      trash.rotation += 0.01;
+  });
+});
