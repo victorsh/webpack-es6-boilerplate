763ba9eed295e80dbc418ee146b2fc78afb38596
Victor Shahbazian <victor.sh.91@gmail.com>
2019-08-01 21:36:17 -0400

    Saved tensorflow example car data. Wrote scripts to save and retrieve api data.

--- src/index.js
+++ src/index.js
@@ -8,62 +8,63 @@ const _ = { join }
 
 process.env.NODE_ENV === 'production' ? console.log('in production') : console.log('in development')
 
-function component () {
-  const element = document.createElement('div')
-  element.innerHTML = _.join(['hello', 'webpack'], ' ')
+// function component () {
+//   const element = document.createElement('div')
+//   element.innerHTML = _.join(['hello', 'webpack'], ' ')
 
-  return element
+//   return element
+// }
+
+// Test.test()
+
+// document.body.appendChild(component())
+
+// PIXISTART.pixelRun()
+
+const getData = async () => {
+  // const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
+  const carsDataReq = await fetch('http://localhost:9000/cardata')
+  const carsData = await carsDataReq.json()
+  const cleaned = carsData.map(car => ({
+    mpg: car.Miles_per_Gallon,
+    horsepower: car.Horsepower
+  }))
+    .filter(car => (car.mpg != null && car.horsepower != null))
+  return cleaned
 }
 
-Test.test()
+const run = async () => {
+  // Load and plot the original input data that we are going to train on.
+  const data = await getData()
+  const values = data.map(d => ({
+    x: d.horsepower,
+    y: d.mpg
+  }))
 
-document.body.appendChild(component())
+  TFVIS.render.scatterplot(
+    { name: 'Horsepower v MPG' },
+    { values },
+    {
+      xLabel: 'Horsepower',
+      yLabel: 'MPG',
+      height: 300
+    }
+  )
 
-PIXISTART.pixelRun()
+  // More code will be added below
+}
 
-// const getData = async () => {
-//   const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
-//   const carsData = await carsDataReq.json()
-//   const cleaned = carsData.map(car => ({
-//     mpg: car.Miles_per_Gallon,
-//     horsepower: car.Horsepower
-//   }))
-//     .filter(car => (car.mpg != null && car.horsepower != null))
-//   return cleaned
-// }
+document.addEventListener('DOMContentLoaded', run)
 
-// const run = async () => {
-//   // Load and plot the original input data that we are going to train on.
-//   const data = await getData()
-//   const values = data.map(d => ({
-//     x: d.horsepower,
-//     y: d.mpg
-//   }))
+const createModel = () => {
+  // Create a sequential model
+  const model = TF.sequential()
 
-//   TFVIS.render.scatterplot(
-//     { name: 'Horsepower v MPG' },
-//     { values },
-//     {
-//       xLabel: 'Horsepower',
-//       yLabel: 'MPG',
-//       height: 300
-//     }
-//   )
+  // Add a single hidden layer
+  model.add(TF.layers.dense({ inputShape: [1], units: 1, useBias: true }))
 
-//   // More code will be added below
-// }
+  // Add an output layer
+  model.add(TF.layers.dense({ units: 1, useBias: true }))
 
-// document.addEventListener('DOMContentLoaded', run)
-
-// const createModel = () => {
-//   // Create a sequential model
-//   const model = TF.sequential()
-
-//   // Add a single hidden layer
-//   model.add(TF.layers.dense({ inputShape: [1], units: 1, useBias: true }))
-
-//   // Add an output layer
-//   model.add(TF.layers.dense({ units: 1, useBias: true }))
-
-//   return model
-// }
+  return model
+}
