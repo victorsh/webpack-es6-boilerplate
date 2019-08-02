e4ed3de426431546102407c2fc825e577ec5db71
Victor Shahbazian <victor.sh.91@gmail.com>
2019-08-01 21:39:37 -0400

    removed tensorflow code. Cleanedup master.

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,17 @@
 const express = require('express')
 const app = express()
+
 const path = require('path')
-const port = 9000
 const fs = require('fs')
 const axios = require('axios')
 const cors = require('cors')
+
+const port = 9000
 
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('hello')
-})
-
-app.get('/cardata', (req, res) => {
-  const data = fs.readFileSync(path.join(__dirname, '/data/cardata.json'), 'utf-8')
-  res.json(JSON.parse(data))
-
-  // axios.get('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
-  //   .then(res => {
-  //     console.log(res.data)
-  //     fs.writeFileSync('./cardata.json', JSON.stringify(res.data))
-  //   })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/index.js
+++ src/index.js
@@ -2,69 +2,19 @@ import join from 'lodash/join'
 import './css/index.scss'
 import * as Test from './scripts/test'
 import * as PIXISTART from './scripts/pixi-starter'
-import * as TF from '@tensorflow/tfjs'
-import * as TFVIS from '@tensorflow/tfjs-vis'
 const _ = { join }
 
 process.env.NODE_ENV === 'production' ? console.log('in production') : console.log('in development')
 
-// function component () {
-//   const element = document.createElement('div')
-//   element.innerHTML = _.join(['hello', 'webpack'], ' ')
+function component () {
+  const element = document.createElement('div')
+  element.innerHTML = _.join(['hello', 'webpack'], ' ')
 
-//   return element
-// }
-
-// Test.test()
-
-// document.body.appendChild(component())
-
-// PIXISTART.pixelRun()
-
-const getData = async () => {
-  // const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
-  const carsDataReq = await fetch('http://localhost:9000/cardata')
-  const carsData = await carsDataReq.json()
-  const cleaned = carsData.map(car => ({
-    mpg: car.Miles_per_Gallon,
-    horsepower: car.Horsepower
-  }))
-    .filter(car => (car.mpg != null && car.horsepower != null))
-  return cleaned
+  return element
 }
 
-const run = async () => {
-  // Load and plot the original input data that we are going to train on.
-  const data = await getData()
-  const values = data.map(d => ({
-    x: d.horsepower,
-    y: d.mpg
-  }))
+Test.test()
 
-  TFVIS.render.scatterplot(
-    { name: 'Horsepower v MPG' },
-    { values },
-    {
-      xLabel: 'Horsepower',
-      yLabel: 'MPG',
-      height: 300
-    }
-  )
+document.body.appendChild(component())
 
-  // More code will be added below
-}
-
-document.addEventListener('DOMContentLoaded', run)
-
-const createModel = () => {
-  // Create a sequential model
-  const model = TF.sequential()
-
-  // Add a single hidden layer
-  model.add(TF.layers.dense({ inputShape: [1], units: 1, useBias: true }))
-
-  // Add an output layer
-  model.add(TF.layers.dense({ units: 1, useBias: true }))
-
-  return model
-}
+PIXISTART.pixelRun()
