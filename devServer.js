42e3ec09b726f78805aa3b78f93dc3d007ca6912
Victor Shahbazian <victor.sh.91@gmail.com>
2019-08-04 16:38:51 -0400

    Added local https, fixed distribution directory.

--- devServer.js
+++ devServer.js
@@ -1,11 +1,21 @@
+const path = require('path')
 const webpack = require('webpack')
 const middleware = require('webpack-dev-middleware')
+const httpsLocalhost = require('https-localhost')
 const express = require('express')
-const app = express()
-const path = require('path')
+
+let app = null
+let config = null
+
+if (process.env.NODE_ENV === 'production') {
+  config = require('./webpack/webpack.prod.js')
+  app = express()
+} else {
+  config = require('./webpack/webpack.dev.js')
+  app = httpsLocalhost()
+}
 
 const port = process.env.PORT
-const config = process.env.NODE_ENV === 'production' ? require('./webpack/webpack.prod.js') : require('./webpack/webpack.dev.js')
 const compiler = webpack(config)
 
 console.log(path.resolve(__dirname, './dist'))
